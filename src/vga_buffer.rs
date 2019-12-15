84b8c0024e345d8ceddadfa3d0dae30c2bcb74d2
gk9339 <dev.9339@hotmail.com>
2019-12-15 19:49:21 +0800

    test testing

--- src/vga_buffer.rs
+++ src/vga_buffer.rs
@@ -3,6 +3,9 @@ use volatile::Volatile;
 use spin::Mutex;
 use lazy_static::lazy_static;
 
+#[cfg(test)]
+use crate::{serial_print, serial_println};
+
 #[allow(dead_code)]
 #[derive(Debug, Clone, Copy, PartialEq, Eq)]
 #[repr(u8)]
@@ -169,4 +172,32 @@ pub fn _print(args: fmt::Arguments)
 {
     use core::fmt::Write;
     WRITER.lock().write_fmt(args).unwrap();
+}
+
+#[test_case]
+fn test_println_many()
+{
+    serial_print!("test_println_many... ");
+    for _ in 0..200
+    {
+        println!("test_println_many output");
+    }
+
+    serial_println!("[ok]");
+}
+
+#[test_case]
+fn test_println_output()
+{
+    serial_print!("test_println_output... ");
+
+    let s = "Some test string that fits on a single line";
+    println!("{}", s);
+    for( i, c ) in s.chars().enumerate()
+    {
+        let screen_char = WRITER.lock().buffer.chars[BUFFER_HEIGHT - 2][i].read();
+        assert_eq!(char::from(screen_char.ascii_character), c);
+    }
+
+    serial_println!("[ok]");
 }
